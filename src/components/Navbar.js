049c20f2fd090c884e40167c36999983760b62ad
Siddhi Agrawal <siddhiagr3008@gmail.com>
2023-12-17 00:46:11 +0530

    Updated to Responsive Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,17 @@ import { Link } from "react-router-dom";
 import { MenuItems } from "./MenuItems";
 
 class Navbar extends Component {
-        render() {
+    state = {clicked: false };
+    handeClick = () =>{
+        this.setState({ clicked: !this.state.clicked})
+     }
+    render() {
         return (
         <nav className="NavbarItems">
             <h1 className="navbar-logo">Pysllium Industry</h1>
+            <div className="menu-icons" onClick={this.handeClick}>
+                <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>                
+            </div>
             <ul className="nav-menu">
                 {MenuItems.map((item, index) =>{
                     return(
@@ -26,3 +33,32 @@ class Navbar extends Component {
 }
 
 export default Navbar;
+
+
+// class Navbar extends Component {
+//     state = {clicked: false };
+//     handeClick = () =>{
+//         this.setState({ clicked: !this.state.clicked})
+//     }
+//     render() {
+//         return (
+//         <nav className="NavbarItems">
+//             <h1 className="navbar-logo">Pysllium Industry</h1>
+//             <div className="menu-icons" onClick={this.handeClick}>
+//                 <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+//             </div>
+//             <ul className={this.state.clicked ? ".nav-menu active" : ".nav-menu"}>
+//                 {MenuItems.map((item, index) =>{
+//                     return(
+//                         <li key={index}>
+//                             <a className={item.cName} href="/">
+//                             {item.title}
+//                             </a>
+//                         </li>
+//                     )
+//                 })}
+//             </ul>
+//         </nav>
+//         );
+//     }
+// }
