a9c16a28b6fec4455f90a66bbcc6ed9612584af7
Siddhi Agrawal <siddhiagr3008@gmail.com>
2023-12-17 00:25:57 +0530

    Added node_modules and package-lock.json file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,24 +5,24 @@ import { Link } from "react-router-dom";
 import { MenuItems } from "./MenuItems";
 
 class Navbar extends Component {
-  render() {
-    return (
-      <nav className="NavbarItems">
-        <h1 className="navbar-logo">Pysllium Industry</h1>
-        <ul className="nav-menu">
-            {MenuItems.map((item, index) =>{
-                return(
-                    <li key={index}>
-                        <a className={item.cName} href="/">
-                        {item.title}
-                        </a>
-                    </li>
-                )
-            })}
-        </ul>
-      </nav>
-    );
-  }
+        render() {
+        return (
+        <nav className="NavbarItems">
+            <h1 className="navbar-logo">Pysllium Industry</h1>
+            <ul className="nav-menu">
+                {MenuItems.map((item, index) =>{
+                    return(
+                        <li key={index}>
+                            <a className={item.cName} href="/">
+                            {item.title}
+                            </a>
+                        </li>
+                    )
+                })}
+            </ul>
+        </nav>
+        );
+    }
 }
 
 export default Navbar;
